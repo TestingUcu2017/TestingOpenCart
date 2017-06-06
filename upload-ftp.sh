468702854f4112266db508d97f33a31b9f2d629e
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-05 22:36:14 -0300

    added upload ftp script

---
+++ upload-ftp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+HOST='waws-prod-sn1-003.ftp.azurewebsites.windows.net '
+USER='$open-cart'
+PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
+FILE='file2.txt'
+
+ftp -vn $HOST <<END_SCRIPT
+quote USER $USER
+quote PASS $PASSWD
+binary
+put $FILE
+quit
+END_SCRIPT
