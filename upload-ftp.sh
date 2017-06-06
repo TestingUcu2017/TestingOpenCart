0cf619ff27dc54d5a3b67cd8ac981502b7f37403
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-06 00:35:55 -0300

    fix upload-ftp scripts

--- upload-ftp.sh
+++ upload-ftp.sh
@@ -2,12 +2,11 @@
 HOST='waws-prod-sn1-003.ftp.azurewebsites.windows.net '
 USER='$open-cart'
 PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
-FILE='file2.txt'
 
 ftp -vn $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
-put ./
+put -r .
 quit
 END_SCRIPT
