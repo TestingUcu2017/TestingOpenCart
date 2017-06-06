97fdf86e686414ae87fd76d2b7a79a3ad577f90c
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-06 00:56:21 -0300

    force yes mput

--- upload-ftp.sh
+++ upload-ftp.sh
@@ -3,7 +3,7 @@ HOST='waws-prod-sn1-003.ftp.azurewebsites.windows.net '
 USER='$open-cart'
 PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
 
-ftp -vn $HOST <<END_SCRIPT
+ftp -ivn $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
