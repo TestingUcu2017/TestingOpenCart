460b58cb227def3082a8d3771ae3057d5ada3d09
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-10 11:33:49 -0300

    fix ftp scripts

--- upload-ftp.sh
+++ upload-ftp.sh
@@ -6,6 +6,18 @@ PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
 ncftp -u $USER -p $PASSWD $HOST <<END_SCRIPT
 passive
 binary
-mput -R ./!(/node_modules)
+mput *.yml
+mput *.xml
+mput *.md
+mput *.json
+mput *.txt
+mput *.sql
+mput *.dist
+mput *.stackdump
+mput *.properties
+mput -R tests/
+mput -R pa11y-master/
+mput -R upload/
+mput -R vaddy-master/
 quit
 END_SCRIPT
