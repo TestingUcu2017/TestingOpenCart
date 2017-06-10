e801f7909d23455996f6e63dd669a2c1a6852b0f
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-10 11:00:04 -0300

    fixed ftp script

--- upload-ftp.sh
+++ upload-ftp.sh
@@ -6,8 +6,8 @@ PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
 ftp -ivn $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
-binary
 passive
-put *.*
+binary
+mput *.*
 quit
 END_SCRIPT
