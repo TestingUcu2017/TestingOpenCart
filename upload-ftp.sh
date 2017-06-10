91943a936e4761a21a5aec144e5eb0f8d674d68b
Rodrigo Suarez <rodrigosrz12@gmail.com>
2017-06-10 10:30:01 -0300

    fixed ftp script

--- upload-ftp.sh
+++ upload-ftp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-HOST='waws-prod-sn1-003.ftp.azurewebsites.windows.net '
+HOST='waws-prod-sn1-003.ftp.azurewebsites.windows.net'
 USER='$open-cart'
 PASSWD='b0v22TwzoPtv7ESsnfyT74xoFZZjPawpRbsuZ9kfd4jGBeaLE4q2wXT7xoXH'
 
@@ -7,6 +7,6 @@ ftp -ivn $HOST <<END_SCRIPT
 quote USER $USER
 quote PASS $PASSWD
 binary
-mput *
+put *.*
 quit
 END_SCRIPT
